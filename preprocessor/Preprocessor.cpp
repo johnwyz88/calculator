ff4d1a78a0708d21d4d69009c0a6cc6cfe07afb8
johnwyz88 <johnwyz88@gmail.com>
2021-06-13 23:14:17 -0700

    Print

--- preprocessor/Preprocessor.cpp
+++ preprocessor/Preprocessor.cpp
@@ -4,6 +4,7 @@
 
 #include <sstream>
 #include <fstream>
+#include <iostream>
 #include "Preprocessor.h"
 
 std::vector<std::string> Preprocessor::preprocess(const std::string &input) {
@@ -11,6 +12,7 @@ std::vector<std::string> Preprocessor::preprocess(const std::string &input) {
     std::stringstream ss(input);
     std::string buf;
     while(std::getline(ss,buf,'\n')){
+        std::cout << buf << std::endl;
         result.push_back(buf);
     }
     return result;
@@ -20,6 +22,7 @@ std::vector<std::string> Preprocessor::preprocess(std::ifstream& input) {
     std::vector<std::string> result;
     std::string buf;
     while(std::getline(input, buf)){
+        std::cout << buf << std::endl;
         result.push_back(buf);
     }
     return result;
